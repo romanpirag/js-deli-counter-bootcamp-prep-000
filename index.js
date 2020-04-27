191d31ce77e6c5e2c9120d3fedc6631e293e6b52
Roman Pirag <mr.anadale@yahoo.com>
2020-04-27 21:42:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const katzDeliLine = []
 
 const takeANumber = (line, name) => {
   line.push(name)
-  return `Welcome, ${name}. You are number ${line.length + 1} in line.`
+  return `Welcome, ${name}. You are number ${line.length} in line.`
 }
 
 const nowServing = (line) => {
