dec48d9c422ce291a700cdb953b415963d2e2284
Roman Pirag <mr.anadale@yahoo.com>
2020-04-27 21:52:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ const currentLine = (line) => {
     return "The line is currently empty."
   }
 for(let i = 0; i < line.length; i++) {
-  newLine.push(`${i+1}.${line[i]}`)
+  newLine.push(` ${i+1}.${line[i]} `)
 }
-return `The line is currently: ${newLine} `
+return `The line is currently: ${newLine}`
 
 }
